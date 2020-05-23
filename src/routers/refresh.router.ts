4ac301fc5dd9ca5d8d932ed40cc304b96405f963
alexbarksdale <ale.barksdale@gmail.com>
2020-05-23 13:03:22 -0700

    [Add] Revoke a refresh token from a user

--- src/routers/refresh.router.ts
+++ src/routers/refresh.router.ts
@@ -8,21 +8,26 @@ export const router = express.Router();
 
 router.post('/auth/refresh', async (req: Request, res: Response) => {
     const token = req.cookies.trident;
-    if (!token) res.send({ ok: false, accessToken: '' });
+    if (!token) res.send({ authenticated: false, accessToken: '' });
 
     let payload: any;
     try {
         payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
     } catch (err) {
-        return res.status(401).send({ ok: false, accessToken: '' });
+        return res.status(401).send({ authenticated: false, accessToken: '' });
     }
 
     // Token is valid
     const user = await UserEntity.findOne({ id: payload.userId });
-    if (!user) return res.status(404).send({ ok: false, accessToken: '' });
+    if (!user) return res.status(404).send({ authenticated: false, accessToken: '' });
+
+    // Verify that the user's token hasn't been revoked
+    if (user.tokenVersion !== payload.tokenVersion) {
+        return res.status(404).send({ authenticated: false, accessToken: '' });
+    }
 
     // User exists, sending tokens
     sendRefreshToken(res, genAccessToken(user));
-    return res.send({ ok: true, accessToken: genAccessToken(user) });
+    return res.send({ authenticated: true, accessToken: genAccessToken(user) });
 });
 
--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,5 +1,6 @@
 import { Response } from 'express';
 import { sign } from 'jsonwebtoken';
+import { getConnection } from 'typeorm';
 
 import { UserEntity } from '../entity/User.entity';
 
@@ -15,7 +16,7 @@ export const genAccessToken = (user: UserEntity) => {
 
 export const genRefreshToken = (user: UserEntity) => {
     return sign(
-        { userId: user.id, userEmail: user.email },
+        { userId: user.id, userEmail: user.email, tokenVersion: user.tokenVersion },
         process.env.REFRESH_TOKEN_SECRET!,
         {
             expiresIn: '5d',
@@ -27,4 +28,29 @@ export const sendRefreshToken = (res: Response, token: string) => {
     res.cookie('trident', token, {
         httpOnly: true,
     });
+};
+
+/*
+ * The UserEntity contains a 'tokenVersion' which we use verify that the user hasn't
+ * had their token revoked. By calling this function, we increase the user's tokenVersion by one
+ * and when the refresh token route (/auth/refresh_token) is called, we check if
+ * the user's 'tokenVersion' is equal to the refresh token's 'tokenVersion'.
+ * If they aren't equal we don't send the user a new refresh token, essentially
+ * revoking their token. The refresh token we generate contains the 'tokenVersion'
+ * which you'll find above in 'genRefreshToken()'. Note that the accessToken does
+ * not contain the tokenVersion.
+ *
+ * Use this function when creating something like forgotPassword or something where
+ * you need to revoke a users's access to your application.
+ */
+export const revokeRefreshToken = async (userId: number): Promise<boolean> => {
+    try {
+        await getConnection()
+            .getRepository(UserEntity)
+            .increment({ id: userId }, 'tokenVersion', 1);
+    } catch (_) {
+        throw new Error('Unable to revoke token from user!');
+    }
+
+    return true;
 };
