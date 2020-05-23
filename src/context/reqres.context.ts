4e1b1d5be2cb82aa7e3825d8719715fc5d7f59ff
alexbarksdale <ale.barksdale@gmail.com>
2020-05-22 18:03:50 -0700

    [Add] Update context for middleware

--- src/context/reqres.context.ts
+++ src/context/reqres.context.ts
@@ -3,4 +3,8 @@ import { Request, Response } from 'express';
 export interface ReqRes {
     req: Request;
     res: Response;
+    payload?: {
+        userId: string;
+        userEmail: string;
+    };
 }
